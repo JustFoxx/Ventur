6095dc274bd93bb9b6976c8f540431c01d8a013a
JustFoxx <justfoxxo@outlook.com>
2022-08-22 22:23:35 +0200

    Mob Attack fix

--- src/main/java/io/github/justfoxx/venturorigin/mixin/minecraft/ActiveTargetGoalMixin.java
+++ src/main/java/io/github/justfoxx/venturorigin/mixin/minecraft/ActiveTargetGoalMixin.java
@@ -1,35 +1,30 @@
 package io.github.justfoxx.venturorigin.mixin.minecraft;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import io.github.justfoxx.venturorigin.Powers;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.ActiveTargetGoal;
 import net.minecraft.entity.ai.goal.TrackTargetGoal;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import org.checkerframework.checker.units.qual.A;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ActiveTargetGoal.class)
-public class ActiveTargetGoalMixin extends TrackTargetGoal {
+public abstract class ActiveTargetGoalMixin extends TrackTargetGoal {
+    @Shadow @Nullable protected LivingEntity targetEntity;
+
     public ActiveTargetGoalMixin(MobEntity mob, boolean checkVisibility) {
         super(mob, checkVisibility);
     }
 
-    @Inject(method = "start", at = @At("HEAD"))
-    public void canTarget(CallbackInfo ci) {
-        if(
-                target instanceof PlayerEntity
-                &&Powers.NO_MOB_ATTACK.isActive(target)
-        ) return;
-    }
-
-    @Override
-    public boolean canStart() {
-        return false;
+    @ModifyArg(method = "start", at = @At(value = "INVOKE", target = "net/minecraft/entity/mob/MobEntity.setTarget (Lnet/minecraft/entity/LivingEntity;)V"))
+    public LivingEntity canTarget(LivingEntity target) {
+        if(Powers.NO_MOB_ATTACK.isActive(target)) {
+            return null;
+        } else {
+            return target;
+        }
     }
 }
