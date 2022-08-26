b0fb7302fe0e11ecb9a60e2e7b5e635fe63a8086
JustFoxx <justfoxxo@outlook.com>
2022-08-26 20:18:07 +0200

    Fixed poppy in Flower Power Added overgown power Forced pehkui Repaired size

--- src/main/java/io/github/justfoxx/venturorigin/Powers.java
+++ src/main/java/io/github/justfoxx/venturorigin/Powers.java
@@ -2,6 +2,9 @@ package io.github.justfoxx.venturorigin;
 
 import io.github.apace100.apoli.power.Power;
 import io.github.apace100.apoli.power.PowerTypeReference;
+import io.github.apace100.apoli.power.factory.PowerFactory;
+import io.github.apace100.calio.data.SerializableData;
+import io.github.justfoxx.venturorigin.powers.SizeChange;
 import net.minecraft.util.Identifier;
 
 import static io.github.justfoxx.venturorigin.Main.ID;
@@ -15,5 +18,9 @@ public class Powers {
     public static final PowerT NO_BLOCK_OFFHAND = new PowerT(ID("paws_no_block_offhand"));
     public static final PowerT NO_MOB_ATTACK = new PowerT(ID("spirit_mobattack"));
 
-    public static final PowerT SLEEP_ON_TREES = new PowerT(ID("green_sleeping"));
+    // public static final PowerT SLEEP_ON_TREES = new PowerT(ID("green_sleeping"));
+
+    public static void register() {
+        Utils.register(Utils.createPower("size_change", data -> SizeChange::new));
+    }
 }
